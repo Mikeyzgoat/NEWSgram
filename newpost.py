d0151358bbcba90578744d1ea7437ca9ba784fa1
Kibutsuji <tadisai091@gmail.com>
2022-06-03 19:54:57 +0530

    Added the news notifier
    there are a couple of changes that are to be made, they are as follows
    - obtain news webpage links in shorter than 64 characters
    - adding icon to the news poster
    - managing latest updates of news, sorted based on time of release.

---
+++ newpost.py
@@ -0,0 +1,48 @@
+from pygooglenews import GoogleNews
+from plyer import notification
+import time
+import random
+# gn will be a dictionary holding all news posted in english
+gn = GoogleNews(lang='en')
+# top news will be filtered from gn dictionary
+tech_top = gn.top_news()
+
+
+# creating a function to remove duplicate news feeds
+
+
+def rem_dupes(stories):
+    for i in range(len(stories)):
+        if stories[i] not in stories[i + 1:]:
+            stories.append(stories[i])
+
+
+# filtering title from all entries
+stories = []
+
+
+def defnotif():
+    stories = []
+
+    for item in tech_top['entries']:
+        stories.append(item.title)
+
+    rem_dupes(stories)
+    stories = sorted(stories)
+    return stories
+# creating the push notfiication function
+
+
+def sendnotif(stories):
+    read_txt = "Read more.."
+    for item in range(len(stories)):
+        if len(stories[item]) <= 64:
+            notification.notify(
+                title=stories[item], message=read_txt, app_icon=None, timeout=15, toast=False)
+            time.sleep(60)
+        else:
+            continue
+    defnotif()
+
+
+sendnotif(defnotif())
